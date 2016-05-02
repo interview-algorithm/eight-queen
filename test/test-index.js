83eb2fe264523e6f4f2377a9a54804dbed64182a
yinyong02 <yinyong02@baidu.com>
2016-05-02 09:56:26 +0800

    test N=1,2,3,4

--- test/test-index.js
+++ test/test-index.js
@@ -1,33 +1,53 @@
 var queen = require('../eight-queen').queen;
 var assert = require('assert');
 
-describe('queen', function() {
-  describe('illegal', function() {
-    it('shoud throw error', function() {
-      assert.throws(function() {
-        queen(-1);
-      });
-    });
-  });
-  describe('calculate', function() {
-    it('N=8 should get 92 results', function() {
-      var q = queen(8);
-      for (var i = 0; i < q.length; ++i) {
+
+function print(q, N) {
+    for (var i = 0; i < q.length; ++i) {
         var matrix = q[i];
-        for (var m = 0; m < 8; ++m) {
-          var line = '';
-          for (var n = 0; n < 8; ++n) {
-            if (matrix[n] === m) {
-              line += ' Q';
-            } else {
-              line += ' *';
+        for (var m = 0; m < N; ++m) {
+            var line = '';
+            for (var n = 0; n < N; ++n) {
+                if (matrix[n] === m) {
+                    line += ' Q';
+                } else {
+                    line += ' *';
+                }
             }
-          }
-          console.log(line);
+            console.log(line);
         }
         console.log('\n');
-      }
-      assert.deepEqual(q.length, 92);
+    }
+}
+
+describe('queen', function () {
+    describe('illegal', function () {
+        it('shoud throw error', function () {
+            assert.throws(function () {
+                queen(-1);
+            });
+        });
     });
-  });
+    describe('calculate', function () {
+        it('N=1 should get 1 results', function () {
+            var q = queen(1);
+            assert.deepEqual(q.length, 1);
+        });
+        it('N=2 should get 0 results', function () {
+            var q = queen(2);
+            assert.deepEqual(q.length, 0);
+        });
+        it('N=3 should get 0 results', function () {
+            var q = queen(3);
+            assert.deepEqual(q.length, 0);
+        });
+        it('N=4 should get 2 results', function () {
+            var q = queen(4);
+            assert.deepEqual(q.length, 2);
+        });
+        it('N=8 should get 92 results', function () {
+            var q = queen(8);
+            assert.deepEqual(q.length, 92);
+        });
+    });
 });
